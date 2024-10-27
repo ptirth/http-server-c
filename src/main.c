f24d81ad150df528bcfdf74ee2ffb24b3b93e594
Tirth Patel <tirthpatel61@outlook.com>
2024-10-27 16:59:33 +0530

    Start a server socket and wait for client request
    Start a TCP server socket on ipv4 address and default protocol. Set
    socket option SO_REUSEADDR to ON. Bind the socket to port 8080 and
    listen for client requests. When client makes a request, accept it using
    accept function. At last immediately close the connection without doing
    anything.
    
    This will let one client connect to the server and exit without any
    response. We can check if the client can successfully connect to server
    or not. The client side will report an error because we are not reading
    the request.

--- src/main.c
+++ src/main.c
@@ -17,17 +17,71 @@
  */
 
 #include "mydefs.h"
+#include <errno.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define SERVER_PORT 8080
+#define SOCK_PROTOCOL 0
+#define SOCKOPT_REUSEADDR 1
+#define CONNECTION_BACKLOG 5
 
 int main(int argc, char *argv[]) {
-  printf(
-      "http-server-c  Copyright (C) 2024  Tirth Patel\n"
-      "This program comes with ABSOLUTELY NO WARRANTY;\n"
-      "This is free software, and you are welcome to redistribute it\n"
-      "under certain conditions; for more details read the copy of\n"
-      "GNU General Public LICENSE that came with the software.\n"
-      "If you did not receive a copy, see <https://www.gnu.org/licenses/>.\n");
+  printf("http-server-c  Copyright (C) 2024  Tirth Patel\n"
+         "This program comes with ABSOLUTELY NO WARRANTY;\n"
+         "This is free software, and you are welcome to redistribute it\n"
+         "under certain conditions; for more details read the copy of\n"
+         "GNU General Public LICENSE that came with the software.\n"
+         "If you did not receive a copy, see "
+         "<https://www.gnu.org/licenses/>.\n\n");
+
+  i32 server_fd = socket(AF_INET, SOCK_STREAM, SOCK_PROTOCOL);
+  if (server_fd == -1) {
+    printf("E: Failed to open server socket: %s\n", strerror(errno));
+    return EXIT_FAILURE;
+  }
+
+  i32 reuseaddr = SOCKOPT_REUSEADDR;
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuseaddr,
+                 sizeof(reuseaddr))) {
+    printf("E: Set socket opt failed: %s\n", strerror(errno));
+    return EXIT_FAILURE;
+  }
+
+  struct sockaddr_in server_addr = {
+      .sin_family = AF_INET,
+      .sin_port = htons(SERVER_PORT),
+      .sin_addr = {htonl(INADDR_ANY)},
+  };
+  if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr))) {
+    printf("E: Bind to port failed: %s\n", strerror(errno));
+    return EXIT_FAILURE;
+  }
+
+  if (listen(server_fd, CONNECTION_BACKLOG)) {
+    printf("E: Listen failed: %s\n", strerror(errno));
+    return EXIT_FAILURE;
+  }
+
+  struct sockaddr_in client_addr;
+  socklen_t client_addr_len = sizeof(client_addr);
+
+  printf("Waiting for client to connect...\n");
+  i32 connection =
+      accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+
+  if (connection == -1) {
+    printf("E: Connection to client failed: %s\n", strerror(errno));
+    return EXIT_FAILURE;
+  }
+  printf("Client connected!\n");
+
+  close(connection);
+  close(server_fd);
 
   return EXIT_SUCCESS;
 }
